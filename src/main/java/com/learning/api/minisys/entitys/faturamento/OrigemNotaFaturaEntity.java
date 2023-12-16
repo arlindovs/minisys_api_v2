3a126fb4b9a62e18c071016b3637dd0c1696e318
Arlindo Silva <arlindovicente@gmail.com>
2023-12-16 11:39:40 -0300

    ligação pedido nota

---
+++ src/main/java/com/learning/api/minisys/entitys/faturamento/OrigemNotaFaturaEntity.java
@@ -0,0 +1,53 @@
+package com.learning.api.minisys.entitys.faturamento;
+
+import com.learning.api.minisys.dtos.faturamento.OrigemNotaFaturaDto;
+import com.learning.api.minisys.entitys.BaseEntity;
+import com.learning.api.minisys.entitys.faturamento.nota_fiscal.NotaFiscalDetalheEntity;
+import com.learning.api.minisys.entitys.faturamento.ordem_servico.OrdemServicoDetalheEntity;
+import com.learning.api.minisys.entitys.faturamento.pedido.PedidoDetalheEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "origem_nota_fatura")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrigemNotaFaturaEntity extends BaseEntity {
+
+    @Column(name = "DATA_CRIACAO")
+    private LocalDateTime dataCriacao;
+
+    @JoinColumn(name = "PEDIDO_DETALHE")
+    @ManyToMany
+    private PedidoDetalheEntity pedidoDetalhe;
+
+    @JoinColumn(name = "ORDEM_SERVICO_DETALHE")
+    @ManyToMany
+    private OrdemServicoDetalheEntity ordemServicoDetalhe;
+
+    @JoinColumn(name = "NOTA_FISCAL_DETALHE")
+    @ManyToMany
+    private NotaFiscalDetalheEntity notaFiscalDetalhe;
+
+
+    public OrigemNotaFaturaEntity(OrigemNotaFaturaDto origemNotaFaturaDto) {
+        this.dataCriacao = LocalDateTime.now();
+        this.pedidoDetalhe = origemNotaFaturaDto.pedidoDetalhe() != null ?
+                new PedidoDetalheEntity(origemNotaFaturaDto.pedidoDetalhe()) : null;
+        this.ordemServicoDetalhe = origemNotaFaturaDto.ordemServicoDetalhe() != null ?
+                new OrdemServicoDetalheEntity(origemNotaFaturaDto.ordemServicoDetalhe()) : null;
+        this.notaFiscalDetalhe = origemNotaFaturaDto.notaFiscalDetalhe() != null ?
+                new NotaFiscalDetalheEntity(origemNotaFaturaDto.notaFiscalDetalhe()) : null;
+    }
+}
