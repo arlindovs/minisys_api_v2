9f5467c14f6c5eb06c465b01bead12212d059436
Arlindo Silva <arlindovicente@gmail.com>
2023-12-29 14:04:53 -0300

    Update UsuarioGrupoEntity.java

--- src/main/java/com/learning/api/minisys/entitys/cadastro/usuario/UsuarioGrupoEntity.java
+++ src/main/java/com/learning/api/minisys/entitys/cadastro/usuario/UsuarioGrupoEntity.java
@@ -81,9 +81,11 @@ public class UsuarioGrupoEntity {
 
     public void setStatusAtivo() {
         this.status = Status.ATIVO;
+        this.versao = LocalDateTime.now();
     }
 
     public void setStatusInativo() {
         this.status = Status.DESATIVADO;
+        this.versao = LocalDateTime.now();
     }
 }
